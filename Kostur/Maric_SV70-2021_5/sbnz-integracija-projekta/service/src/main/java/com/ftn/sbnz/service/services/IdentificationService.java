2c5c766cd50bab48918737adf4861020db8c0512
maric300 <branko.maric300@gmail.com>
2025-09-25 06:19:39 +0200

    feat: added longterm and temprary sessions in order for CEP to work

--- Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/IdentificationService.java
+++ Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/IdentificationService.java
@@ -1,5 +1,7 @@
 package com.ftn.sbnz.service.services;
 
+import com.ftn.sbnz.model.enums.Accessibility;
+import com.ftn.sbnz.model.enums.Difficulty;
 import com.ftn.sbnz.model.enums.Luster;
 import com.ftn.sbnz.model.enums.Transparency;
 import com.ftn.sbnz.model.models.Mineral;
@@ -20,41 +22,49 @@ import java.util.stream.Collectors;
 public class IdentificationService {
 
     private final KieContainer kieContainer;
-    private final List<Mineral> mineralDatabase;
+    private final KieSession cepKieSession;
 
     @Autowired
     public IdentificationService(KieContainer kieContainer) {
         this.kieContainer = kieContainer;
-        // Temporary Mock database
-        this.mineralDatabase = new ArrayList<>();
-        this.mineralDatabase.add(new Mineral(1L, "Serpentine", Arrays.asList("green", "black"), Transparency.TRANSLUCENT, Luster.GREASY, 2.5, 4.0, "white", Arrays.asList("serpentinite"), Arrays.asList("Zlatibor", "Kopaonik")));
-        this.mineralDatabase.add(new Mineral(2L, "Malachite", Arrays.asList("green"), Transparency.OPAQUE, Luster.GLASSY, 3.5, 4.0, "green", Arrays.asList("limestone"), Arrays.asList("Rudnik")));
-        this.mineralDatabase.add(new Mineral(3L, "Amethyst (Quartz)", Arrays.asList("purple"), Transparency.TRANSPARENT, Luster.GLASSY, 7.0, 7.0, "white", Arrays.asList("volcanic rock"), Arrays.asList("Fruška gora")));
-        this.mineralDatabase.add(new Mineral(4L, "Rock Crystal (Quartz)", Arrays.asList("colorless"), Transparency.TRANSPARENT, Luster.GLASSY, 7.0, 7.0, "white", Arrays.asList("granite", "pegmatite"), Arrays.asList("Kopaonik")));
+        this.cepKieSession = kieContainer.newKieSession("cepKieSession");
     }
 
     public List<IdentificationResult> identifyMineral(Sample sample) {
-        KieSession kieSession = kieContainer.newKieSession();
+        KieSession identificationSession = kieContainer.newKieSession("rulesKSession");
 
         List<IdentificationResult> results = new ArrayList<>();
-        kieSession.setGlobal("results", results);
+        identificationSession.setGlobal("results", results);
+        identificationSession.setGlobal("cepSession", this.cepKieSession);
 
-        kieSession.insert(sample);
-
-        for (Mineral mineral : this.mineralDatabase) {
-            kieSession.insert(mineral);
+        identificationSession.insert(sample);
+        for (Mineral mineral : this.getMineralDatabase()) {
+            identificationSession.insert(mineral);
         }
 
-        System.out.println("Firing rules...");
-        kieSession.fireAllRules();
-        System.out.println("Rules have been fired.");
+        System.out.println("Firing identification rules...");
+        identificationSession.fireAllRules();
+        System.out.println("Identification rules have been fired.");
+        identificationSession.dispose(); // Uništavamo privremenu sesiju
 
-        kieSession.dispose();
+        new Thread(() -> {
+            System.out.println("Firing CEP rules in the background...");
+            this.cepKieSession.fireAllRules();
+        }).start();
 
-        // Sorting the results in descending order by score
         return results.stream()
                 .sorted(Comparator.comparingInt(IdentificationResult::getScore).reversed())
                 .collect(Collectors.toList());
+    }
+
+    // Mock baza podataka
+    private List<Mineral> getMineralDatabase() {
+        return Arrays.asList(
+                new Mineral(1L, "Serpentine", Arrays.asList("green", "black"), Transparency.TRANSLUCENT, Luster.GREASY, 2.5, 4.0, "white", Arrays.asList("serpentinite"), Arrays.asList("Zlatibor", "Kopaonik"), Accessibility.ON_FOOT, Difficulty.MEDIUM),
+                new Mineral(2L, "Malachite", Arrays.asList("green"), Transparency.OPAQUE, Luster.GLASSY, 3.5, 4.0, "green", Arrays.asList("limestone"), Arrays.asList("Rudnik"), Accessibility.BY_CAR, Difficulty.MEDIUM),
+                new Mineral(3L, "Amethyst (Quartz)", Arrays.asList("purple"), Transparency.TRANSPARENT, Luster.GLASSY, 7.0, 7.0, "white", Arrays.asList("volcanic rock"), Arrays.asList("Fruška gora"), Accessibility.BY_CAR, Difficulty.HARD),
+                new Mineral(4L, "Rock Crystal (Quartz)", Arrays.asList("colorless"), Transparency.TRANSPARENT, Luster.GLASSY, 7.0, 7.0, "white", Arrays.asList("granite", "pegmatite"), Arrays.asList("Kopaonik"),Accessibility.BY_CAR, Difficulty.MEDIUM)
+        );
     }
 }
 
