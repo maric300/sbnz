07ce522f809336371e930d8422d29dfbe8c00669
maric300 <branko.maric300@gmail.com>
2025-10-04 02:45:03 +0200

    feat: separated IdentificationService to interface and implementation

---
+++ Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/services/IIdentificationService.java
@@ -0,0 +1,13 @@
+package com.ftn.sbnz.service.services;
+
+import com.ftn.sbnz.model.models.IdentificationResult;
+import com.ftn.sbnz.model.models.Mineral;
+import com.ftn.sbnz.model.models.Sample;
+
+import java.util.List;
+
+public interface IIdentificationService {
+    List<IdentificationResult> identifyMineral(Sample sample);
+
+    List<Mineral> getMineralDatabase();
+}
