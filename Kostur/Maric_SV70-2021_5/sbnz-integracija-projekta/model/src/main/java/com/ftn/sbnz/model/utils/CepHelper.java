22d144da4d3b852d26af998dfcfc6bdc27d71594
maric300 <branko.maric300@gmail.com>
2025-10-05 20:32:08 +0200

    added new methods cepHelper

--- Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/utils/CepHelper.java
+++ Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/model/src/main/java/com/ftn/sbnz/model/utils/CepHelper.java
@@ -3,6 +3,8 @@ package com.ftn.sbnz.model.utils;
 import com.ftn.sbnz.model.events.PotentialCorrectionEvent;
 import com.ftn.sbnz.model.events.PotentialDiscoveryEvent;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class CepHelper {
 
@@ -26,5 +28,31 @@ public class CepHelper {
                 .map(PotentialCorrectionEvent::getUserId)
                 .distinct()
                 .count() >= 5;
+    }
+
+    public static boolean isSeasonalSpike(Map<String, Object> analysisData) {
+        if (analysisData == null || analysisData.isEmpty()) {
+            return false;
+        }
+
+        // Uslov 1: Obrazac se ponovio najmanje dve uzastopne godine.
+        Set<Integer> years = (Set<Integer>) analysisData.get("years");
+        if (years == null || years.size() < 2) {
+            return false;
+        }
+
+        // Uslov 2: Mora postojati bar neki prosek van sezone za poređenje.
+        Double offPeakAvg = (Double) analysisData.get("offPeakAvg");
+        if (offPeakAvg == null || offPeakAvg <= 0) {
+            return false;
+        }
+
+        // Uslov 3: Prosečan broj pronalazaka u sezoni mora biti 250% veći.
+        Double peakAvg = (Double) analysisData.get("peakAvg");
+        if (peakAvg == null || peakAvg <= (offPeakAvg * 2.5)) {
+            return false;
+        }
+
+        return true;
     }
 }
