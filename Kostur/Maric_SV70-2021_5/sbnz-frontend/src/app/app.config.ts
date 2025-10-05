a8d388fc7d00196608fc33fbde99db9284fdf94a
maric300 <branko.maric300@gmail.com>
2025-10-05 02:30:59 +0200

    added auth guard

--- Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.config.ts
+++ Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.config.ts
@@ -2,13 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-import {provideHttpClient} from "@angular/common/http";
+import {provideHttpClient, withInterceptors} from "@angular/common/http";
+import {tokenInterceptor} from "./interceptors/token.interceptor";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([tokenInterceptor])),
   ]
 };
--- Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.routes.ts
+++ Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.routes.ts
@@ -2,9 +2,19 @@ import { Routes } from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
+import {authGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
-  {path: 'home', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+
+  {
+    path: 'home',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+
+  {path: '', redirectTo: 'login', pathMatch: 'full'},
+
+  {path: '**', redirectTo: 'login'}
 ];
