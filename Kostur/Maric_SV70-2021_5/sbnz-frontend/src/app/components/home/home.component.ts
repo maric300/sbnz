8ec454e2fcfd92a551eacf56a4b15e89002d851d
maric300 <branko.maric300@gmail.com>
2025-10-05 04:34:14 +0200

    added notification number on bell icon

--- Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/components/home/home.component.ts
+++ Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/components/home/home.component.ts
@@ -12,6 +12,7 @@ import {IdentificationResult} from "../../models/identificaion.result";
 import {Mineral} from "../../models/mineral";
 import {Transparency} from "../../enums/transparency";
 import {Luster} from "../../enums/luster";
+import {NotificationService} from "../../services/notification.service";
 
 @Component({
   selector: 'app-home',
@@ -24,7 +25,9 @@ export class HomeComponent {
   http = inject(HttpClient);
   router = inject(Router);
   authService = inject(AuthService);
+  notificationService = inject(NotificationService);
   apiBaseUrl = 'http://localhost:8080/api';
+
 
   activeTab = signal<'identification' | 'hierarchy'>('identification');
   isLoading = signal(false);
@@ -40,6 +43,14 @@ export class HomeComponent {
 
   identificationResults = signal<IdentificationResult[]>([]);
   hierarchyResults = signal<Mineral[]>([]);
+
+  ngOnInit(): void {
+    // Kada se komponenta učita, ako je korisnik admin,
+    // odmah dohvatamo broj nepročitanih notifikacija.
+    if (this.authService.isAdmin()) {
+      this.notificationService.fetchUnreadCount().subscribe();
+    }
+  }
 
   submitIdentification(): void {
     this.isLoading.set(true);
