a062c9285a98a8563bb84dcf121ec36553e41114
maric300 <branko.maric300@gmail.com>
2025-10-05 03:41:07 +0200

    updated notifications to app routes

--- Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.routes.ts
+++ Kostur/Maric_SV70-2021_5/sbnz-frontend/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import {HomeComponent} from "./components/home/home.component";
 import {LoginComponent} from "./components/login/login.component";
 import {RegisterComponent} from "./components/register/register.component";
 import {authGuard} from "./guards/auth.guard";
+import {NotificationsComponent} from "./components/notifications/notifications.component";
 
 export const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
+  {path: 'notifications', component: NotificationsComponent},
 
   {
     path: 'home',
