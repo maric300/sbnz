ddc002be89a902d66fb4b88292732a49e1a138df
maric300 <branko.maric300@gmail.com>
2025-10-05 21:55:39 +0200

    added annotation in SecurityConfig to fix a bug with preAuthorize annotation

--- Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/SecurityConfig.java
+++ Kostur/Maric_SV70-2021_5/sbnz-integracija-projekta/service/src/main/java/com/ftn/sbnz/service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -19,6 +20,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final JwtAuthFilter jwtAuthFilter;
